5ea66cc5399d4291d56cb3f858fb06e69741be4b
Joey <zzying77@qq.com>
2020-04-28 10:34:35 +0800

    旋转拍照保存image

--- LFCameraPickerControllerDEMO/LFCameraPickerControllerDEMO/class/Category/UIImage/UIImage+LFCamera_Orientation.m
+++ LFCameraPickerControllerDEMO/LFCameraPickerControllerDEMO/class/Category/UIImage/UIImage+LFCamera_Orientation.m
@@ -90,102 +90,62 @@
     return img;
 }
 
-- (UIImage *)easyRotateImageOrientation:(UIImageOrientation)orient
+- (UIImage *)easyRotateImageOrientation:(UIImageOrientation)orientation
 {
-    CGRect bnds = CGRectZero;
-    UIImage* copy = nil;
-    @autoreleasepool {
-        
-        CGImageRef imag = self.CGImage;
-        CGRect rect = CGRectZero;
-        CGAffineTransform tran = CGAffineTransformIdentity;
-        
-        rect.size.width = CGImageGetWidth(imag);
-        rect.size.height = CGImageGetHeight(imag);
-        
-        bnds = rect;
-        
-        switch (orient)
-        {
-            case UIImageOrientationUp:
-                return self;
-                
-            case UIImageOrientationUpMirrored:
-                tran = CGAffineTransformMakeTranslation(rect.size.width, 0.0);
-                tran = CGAffineTransformScale(tran, -1.0, 1.0);
-                break;
-                
-            case UIImageOrientationDown:
-                tran = CGAffineTransformMakeTranslation(rect.size.width,
-                                                        rect.size.height);
-                tran = CGAffineTransformRotate(tran, M_PI);
-                break;
-                
-            case UIImageOrientationDownMirrored:
-                tran = CGAffineTransformMakeTranslation(0.0, rect.size.height);
-                tran = CGAffineTransformScale(tran, 1.0, -1.0);
-                break;
-                
-            case UIImageOrientationLeft:
-                bnds = easySwapWidthAndHeight(bnds);
-                tran = CGAffineTransformMakeTranslation(0.0, rect.size.width);
-                tran = CGAffineTransformRotate(tran, 3.0 * M_PI / 2.0);
-                break;
-                
-            case UIImageOrientationLeftMirrored:
-                bnds = easySwapWidthAndHeight(bnds);
-                tran = CGAffineTransformMakeTranslation(rect.size.height,
-                                                        rect.size.width);
-                tran = CGAffineTransformScale(tran, -1.0, 1.0);
-                tran = CGAffineTransformRotate(tran, 3.0 * M_PI / 2.0);
-                break;
-                
-            case UIImageOrientationRight:
-                bnds = easySwapWidthAndHeight(bnds);
-                tran = CGAffineTransformMakeTranslation(rect.size.height, 0.0);
-                tran = CGAffineTransformRotate(tran, M_PI / 2.0);
-                break;
-                
-            case UIImageOrientationRightMirrored:
-                bnds = easySwapWidthAndHeight(bnds);
-                tran = CGAffineTransformMakeScale(-1.0, 1.0);
-                tran = CGAffineTransformRotate(tran, M_PI / 2.0);
-                break;
-                
-            default:
-                return self;
-        }
-        
-        UIGraphicsBeginImageContext(bnds.size);
-        
-        CGContextRef ctxt = UIGraphicsGetCurrentContext();
-        
-        switch (orient)
-        {
-            case UIImageOrientationLeft:
-            case UIImageOrientationLeftMirrored:
-            case UIImageOrientationRight:
-            case UIImageOrientationRightMirrored:
-                CGContextScaleCTM(ctxt, -1.0, 1.0);
-                CGContextTranslateCTM(ctxt, -rect.size.height, 0.0);
-                break;
-                
-            default:
-                CGContextScaleCTM(ctxt, 1.0, -1.0);
-                CGContextTranslateCTM(ctxt, 0.0, -rect.size.height);
-                break;
-        }
-        
-        CGContextConcatCTM(ctxt, tran);
-        CGContextDrawImage(UIGraphicsGetCurrentContext(), rect, imag);
-        CGContextRelease(ctxt);
-        copy = UIGraphicsGetImageFromCurrentImageContext();
-        
-        
-        UIGraphicsEndImageContext();
+    long double rotate = 0.0;
+    CGRect rect;
+    float translateX = 0;
+    float translateY = 0;
+    float scaleX = 1.0;
+    float scaleY = 1.0;
+
+
+    switch (orientation) {
+        case UIImageOrientationLeft:
+            rotate = M_PI_2;
+            rect = CGRectMake(0, 0, self.size.height, self.size.width);
+            translateX = 0;
+            translateY = -rect.size.width;
+            scaleY = rect.size.width/rect.size.height;
+            scaleX = rect.size.height/rect.size.width;
+            break;
+        case UIImageOrientationRight:
+            rotate = 3 * M_PI_2;
+            rect = CGRectMake(0, 0, self.size.height, self.size.width);
+            translateX = -rect.size.height;
+            translateY = 0;
+            scaleY = rect.size.width/rect.size.height;
+            scaleX = rect.size.height/rect.size.width;
+            break;
+        case UIImageOrientationDown:
+            rotate = M_PI;
+            rect = CGRectMake(0, 0, self.size.width, self.size.height);
+            translateX = -rect.size.width;
+            translateY = -rect.size.height;
+            break;
+        default:
+            rotate = 0.0;
+            rect = CGRectMake(0, 0, self.size.width, self.size.height);
+            translateX = 0;
+            translateY = 0;
+            break;
     }
-    
-    return copy;
+
+    UIGraphicsBeginImageContext(rect.size);
+    CGContextRef context = UIGraphicsGetCurrentContext();
+
+    //做CTM变换
+    CGContextTranslateCTM(context, 0.0, rect.size.height);
+    CGContextScaleCTM(context, 1.0, -1.0);
+    CGContextRotateCTM(context, rotate);
+    CGContextTranslateCTM(context, translateX, translateY);
+    CGContextScaleCTM(context, scaleX, scaleY);
+
+    //绘制图片
+    CGContextDrawImage(context, CGRectMake(0, 0, rect.size.width, rect.size.height), self.CGImage);
+    UIImage *newPic = UIGraphicsGetImageFromCurrentImageContext();
+
+    return newPic;
 }
 
 /** 交换宽和高 */
